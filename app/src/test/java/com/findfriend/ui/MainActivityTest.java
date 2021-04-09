64d26993e7fba0ac57fd862be1aa2a5e2dd09dad
anutino <fokeevanna@gmail.com>
2021-04-09 14:47:40 +0300

    add test

---
+++ app/src/test/java/com/findfriend/ui/MainActivityTest.java
@@ -0,0 +1,25 @@
+package com.findfriend.ui;
+
+import com.findfriend.ui.activity.MainActivity;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+
+    @Before
+    public void setup() {
+        Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void  addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
